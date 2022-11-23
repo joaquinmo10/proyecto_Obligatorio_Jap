9226e0faa7755f868a2a194997e89a88cbf2e10c
joaquinmo10 <108738565+joaquinmo10@users.noreply.github.com>
2022-11-23 01:37:09 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,2 +1,31 @@
 let Sesion = localStorage.getItem("Usuario");
 document.getElementById("Sesion").innerHTML = Sesion;
+
+document.getElementById("email").value = localStorage.getItem("Email")
+
+function userSav(){
+    let userInfo = {
+      firstName: document.getElementById("firstName").value,
+      secondName: document.getElementById("secondName").value,
+      surname: document.getElementById("surname").value,
+      secondSurname: document.getElementById("surname2").value,
+      phone: document.getElementById("phone").value,
+    };
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+  };
+
+function infoSaved(){
+    return JSON.parse(localStorage.getItem("userInfo"));
+}
+function showInfoSav(){
+    if (infoSaved()) {
+      let info = infoSaved();
+      document.getElementById("firstName").value = info.firstName;
+      document.getElementById("secondName").value = info.secondName;
+      document.getElementById("surname").value = info.surname;
+      document.getElementById("surname2").value = info.secondSurname;
+      document.getElementById("phone").value = info.phone;
+    }
+  };
+  
+  showInfoSav();
